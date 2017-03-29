6b5e5ac0cceceaf336d3575946282616bee1e837
Sam Lim <limsammy1@gmail.com>
2017-03-29 11:32:52 -0600

    refactored sum method, added helper method

--- lib/card_check.rb
+++ lib/card_check.rb
@@ -10,12 +10,17 @@ class CardCheck
     @card_type = 'American Express' if card_number.to_s[0..2] == '342'
   end
 
-  def sum(card_number=@card_number)
-    final = Array.new
+  def format_card(card_number=@card_number)
     card_number = *(card_number.split('').map { |num| num.to_i })
     card_number = card_number.reverse
     card_number.map!.with_index { |num, index| index.even? ? num : num * 2 }
-    card_number.each do |num|
+  end
+
+  def sum(card_number=@card_number)
+    final = Array.new
+    formatted_num = format_card(card_number)
+
+    formatted_num.each do |num|
       if num > 9
         first,second = num.to_s.split('').map { |i| i.to_i }
         final.push(first + second)
