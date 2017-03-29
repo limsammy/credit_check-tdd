3cf22ac2e66c757ba1a59c5be69c85431e6a5adb
Sam Lim <limsammy1@gmail.com>
2017-03-29 11:31:09 -0600

    added sum method, passes test

--- lib/card_check.rb
+++ lib/card_check.rb
@@ -9,4 +9,21 @@ class CardCheck
   def check_card_type(card_number=@card_number)
     @card_type = 'American Express' if card_number.to_s[0..2] == '342'
   end
+
+  def sum(card_number=@card_number)
+    final = Array.new
+    card_number = *(card_number.split('').map { |num| num.to_i })
+    card_number = card_number.reverse
+    card_number.map!.with_index { |num, index| index.even? ? num : num * 2 }
+    card_number.each do |num|
+      if num > 9
+        first,second = num.to_s.split('').map { |i| i.to_i }
+        final.push(first + second)
+      else
+        final.push(num)
+      end
+    end
+
+    final.inject(:+)
+  end
 end
